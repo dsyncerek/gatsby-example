b06fa24a94be70ab37450804e17777a640e6683d
Dariusz Syncerek <dsyncerek@outlook.com>
2019-04-02 20:48:52 +0200

    Fix Marquee

--- src/components/Marquee/index.js
+++ src/components/Marquee/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import { MarqueeContainerStyled, MarqueeItemStyled, MarqueeStyled } from './styled';
+import { MarqueeContainerStyled, MarqueeItemInnerStyled, MarqueeItemStyled, MarqueeStyled } from './styled';
 
-const Marquee = ({ items = [] }) => (
-  <MarqueeStyled>
-    <MarqueeContainerStyled itemsLength={items.length}>
-      {[...items, ...items].map(item => (
-        <MarqueeItemStyled>
-          {item}
-        </MarqueeItemStyled>
-      ))}
-    </MarqueeContainerStyled>
-  </MarqueeStyled>
-);
+const Marquee = ({ items = [] }) => {
+  const itemWidth = 300;
+
+  return (
+    <MarqueeStyled>
+      <MarqueeContainerStyled itemsLength={items.length} itemWidth={itemWidth}>
+        {[...items, ...items].map(item => (
+          <MarqueeItemStyled width={itemWidth}>
+            <MarqueeItemInnerStyled>
+              {item}
+            </MarqueeItemInnerStyled>
+          </MarqueeItemStyled>
+        ))}
+      </MarqueeContainerStyled>
+    </MarqueeStyled>
+  );
+};
 
 export default Marquee;
