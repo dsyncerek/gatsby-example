ee6603a371b48c00b14b717aee340b60cf2551c9
Dariusz Syncerek <dsyncerek@outlook.com>
2019-07-08 09:38:00 +0200

    Add stylelint and eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,16 @@ module.exports = {
       options: { rule: { include: /\.svg$/ } },
     },
     {
-      resolve: `gatsby-plugin-styled-components`,
+      resolve: 'gatsby-plugin-styled-components',
       options: {},
     },
+    // {
+    //   resolve: 'gatsby-plugin-eslint',
+    //   options: {},
+    // },
+    // {
+    //   resolve: 'gatsby-plugin-stylelint',
+    //   options: { files: ['**/*.{js,jsx}'] },
+    // },
   ],
 };
